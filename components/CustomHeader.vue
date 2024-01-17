8eeb004cbe02ee6ea6fb7baf6ddff60e056db5ba
Andrei Maslau <30477400+andrey-maslov@users.noreply.github.com>
2024-01-17 14:54:17 +0000

    buttons updated

--- components/CustomHeader.vue
+++ components/CustomHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex items-center justify-between w-full gap-1 px-4 bg-white">
+  <header class="flex items-center justify-between w-full gap-1 px-6 bg-white">
     <CustomLogo />
     <div class="flex items-center gap-2 md:gap-5">
       <a class="hidden md:inline-block" href="https://adcatch.pro/#/registration">
--- layouts/default.vue
+++ layouts/default.vue
@@ -16,14 +16,14 @@
 <style lang="scss" scoped>
 .app {
   position: relative;
-  padding-top: 80px;
+  padding-top: 96px;
 
   header {
     position: fixed;
     top: 0;
     width: 100%;
     box-shadow: 0 4px 4px 0 #00000040;
-    height: 80px;
+    height: 96px;
     z-index: 200;
   }
 }
